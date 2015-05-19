8e6a99c22301aaf61caf3dc5536bcba6de54fd7e
S.Ponselvi Christo <ChristoJoel@SPonselvis-MacBook.local>
2015-05-19 12:33:47 +0530

    Initial Commit

---
+++ git/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  git
+//
+//  Created by S.Ponselvi Christo on 19/05/15.
+//  Copyright (c) 2015 S.Ponselvi Christo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ git/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  git
+//
+//  Created by S.Ponselvi Christo on 19/05/15.
+//  Copyright (c) 2015 S.Ponselvi Christo. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
